95512a94a529e1fc5cda4c5d7c18d27b34248600
vikramr2 <vikramr2@illinois.edu>
2023-06-25 04:56:17 +0000

    begin restructure to classes

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
+from tau import TAU
 
 data = np.load('TAU_partition_example.graph.npy')
 print(data)
 print(len(data))
-print(list(enumerate(data)))
+print(list(zip(list(range(1,len(data)+1)), data)))
